c14829ed34144a74525d5e93b031dbce720bc83f
Vinzent Brömauer <vinz.corno@web.de>
2023-07-21 12:11:53 +0200

    minor cosmetic change

--- src/lib/LoadEpub.svelte
+++ src/lib/LoadEpub.svelte
@@ -15,6 +15,8 @@
 <div style="margin-bottom: 5px;">
     <!--This is ugly but hides the even uglier file selected text-->
     <input type="file" id="selectedFile" accept=".epub" style="display: none;" bind:files />
-    <input type="button" value="Open Ebook" on:click={openFileClick} />
-    <input type="button" value="Close Ebook" on:click={() => epubFile = null} />
+    <input type="button" value="open ebook" on:click={openFileClick} />
+    {#if epubFile !== null}
+        <input type="button" value="close ebook" on:click={() => (epubFile = null)} />
+    {/if}
 </div>
