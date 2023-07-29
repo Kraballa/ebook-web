b653cc6618527c1c1c50805f3cdf0bef40af47e6
Vinzent Brömauer <vinz.corno@web.de>
2023-07-29 13:31:31 +0200

    move loading logic to loadEpub component

--- src/App.svelte
+++ src/App.svelte
@@ -3,34 +3,18 @@
     import Spine from "./lib/Spine.svelte";
     import Chapter from "./lib/Chapter.svelte";
     import Navigator from "./lib/Navigator.svelte";
-    import JSZip from "jszip";
-    import { parseSpine } from "./lib/parseSpine";
     import ProgressHint from "./lib/ProgressHint.svelte";
     import Info from "./lib/Info.svelte";
 
-    let epubFile = null;
+    let epubZip = null;
     let epubChapters = [];
     let chapterIndex = 0;
 
-    let epubZip = null;
-
-    $: {
-        if (epubFile) {
-            loadFile();
-        }
-    }
-
-    async function loadFile() {
-        chapterIndex = 0;
-        const buf = epubFile.arrayBuffer();
-        epubZip = await JSZip.loadAsync(buf);
-        epubChapters = await parseSpine(epubZip);
-    }
 </script>
 
 <div class="row bg">
-    <LoadEpub bind:epubFile />
-    {#if epubFile}
+    <LoadEpub bind:epubZip bind:epubChapters bind:chapterIndex/>
+    {#if epubZip}
         <Spine bind:chapterIndex bind:epubChapters />
         <Navigator bind:chapterIndex bind:epubChapters />
         <Chapter bind:epubZip bind:epubChapters bind:chapterIndex />
--- src/lib/LoadEpub.svelte
+++ src/lib/LoadEpub.svelte
@@ -1,23 +1,45 @@
 <script>
-    export let epubFile;
-    let files = [];
+    import JSZip from "jszip";
+    import { parseSpine } from "../lib/parseSpine";
+
+    export let chapterIndex = 0;
+    export let epubZip = null;
+    export let epubChapters = [];
+
+    let inputElement;
+    let files = null;
+
     $: {
         if (files && files[0]) {
-            epubFile = files[0];
+            loadFile(files[0]);
         }
     }
 
     function openFileClick() {
         document.getElementById("selectedFile").click();
     }
+
+    function closeFile() {
+        epubZip = null;
+        inputElement.value = "";
+    }
+
+    async function loadFile(file) {
+        if(chapterIndex !== 0){
+            chapterIndex = 0;
+        }
+        const buf = file.arrayBuffer();
+        epubZip = await JSZip.loadAsync(buf);
+        epubChapters = await parseSpine(epubZip);
+    }
 </script>
 
 <div style="margin-bottom: 5px;">
     <!--This is ugly but hides the even uglier file selected text-->
-    <input type="file" id="selectedFile" accept=".epub" style="display: none;" bind:files />
+    <input type="file" id="selectedFile" accept=".epub" style="display: none;" bind:files bind:this={inputElement} />
     <input type="button" value="open ebook" on:click={openFileClick} />
-    {#if epubFile !== null}
-        <input type="button" value="close ebook" on:click={() => (epubFile = null)} />
+    {#if files !== null}
+        <input type="button" value="close ebook" on:click={closeFile} />
     {/if}
     <a class="right" href="https://kraballa.net">home</a>
     <a class="right" href="https://github.com/Kraballa/ebook-web">github repo</a>
