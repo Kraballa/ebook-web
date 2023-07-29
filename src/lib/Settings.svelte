700ad8ba60627ab332bae0d69a23d47d1dbfc784
Vinzent Brömauer <vinz.corno@web.de>
2023-07-29 13:33:58 +0200

    settings scaffolding

---
+++ src/lib/Settings.svelte
@@ -0,0 +1,56 @@
+<script>
+    let showMetadata = true;
+    let minimalMode = false;
+    let autoBookmark = false;
+</script>
+
+<h3>Settings</h3>
+
+<table>
+    <tr>
+        <td>
+            <label>
+                <input type="checkbox" bind:checked={autoBookmark}/>
+                automatic bookmark
+            </label>
+        </td>
+        <td>
+            <div>Automatically create bookmarks based on the book title. Stores chapter and scroll percentage in <a href="https://en.wikipedia.org/wiki/Web_storage">local storage</a></div>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>
+                <input type="checkbox" bind:checked={showMetadata} />
+                show metadata
+            </label>
+        </td>
+        <td>
+            <div>Show metadata of the ebook if available.</div>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <label>
+                <input type="checkbox" bind:checked={minimalMode} />
+                use minimal mode
+            </label>
+        </td>
+        <td>
+            <div>
+                Discard embedded stylesheets and images leaving only html.
+            </div>
+        </td>
+    </tr>
+</table>
+
+<style>
+
+    td {
+        min-width: 20vw;
+    }
+
+    div {
+        text-align: justify;
+    }
+</style>
